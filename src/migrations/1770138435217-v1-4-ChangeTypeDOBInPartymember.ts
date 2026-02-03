ed88d2c003d842d6d13b52a9f01c16461b06994f
Felix <sontug9xx@gmail.com>
2026-02-04 00:08:51 +0700

    ChangeDataTypeInDOB

---
+++ src/migrations/1770138435217-v1-4-ChangeTypeDOBInPartymember.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class V14ChangeTypeDOBInPartymember1770138435217 implements MigrationInterface {
+    name = 'V14ChangeTypeDOBInPartymember1770138435217'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "party_members" DROP COLUMN "date_of_birth"`);
+        await queryRunner.query(`ALTER TABLE "party_members" ADD "date_of_birth" date`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "party_members" DROP COLUMN "date_of_birth"`);
+        await queryRunner.query(`ALTER TABLE "party_members" ADD "date_of_birth" TIMESTAMP`);
+    }
+
+}
