4cda7aa224206c44369eb9adbc74457e161180a7
Felix <sontug9xx@gmail.com>
2026-02-11 01:19:03 +0700

    FixMailModule3.0

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,20 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { TransformInterceptor } from './common/transform.interceptor';
 import { HttpExceptionFilter } from './common/http-exception.filter';
+// 👇 1. Import cái này vào
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 👇 2. CẤU HÌNH VALIDATION (QUAN TRỌNG NHẤT)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Tự động xóa các trường thừa không có trong DTO
+      forbidNonWhitelisted: true, // Báo lỗi nếu gửi trường thừa (Optional)
+      transform: true, // Tự động convert data (vd: string '1' -> number 1)
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('FPTU-DPC2-SwaagerAPILists')
@@ -16,9 +27,11 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
+
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
-  app.enableCors();
+
+  app.enableCors(); // Bật CORS để Frontend gọi được API
 
   await app.listen(3000);
 }
