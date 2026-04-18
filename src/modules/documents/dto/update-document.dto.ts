04544855d6aaf5f457f7b47b6b7b18a4b22724eb
anhncdhe181098 <145757300+danhhhm@users.noreply.github.com>
2026-04-18 23:52:32 +0700

    update role for document category

--- src/modules/documents/dto/update-document.dto.ts
+++ src/modules/documents/dto/update-document.dto.ts
@@ -1,4 +1,16 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateDocumentDto } from './create-document.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
+import { Transform } from 'class-transformer';
 
-export class UpdateDocumentDto extends PartialType(CreateDocumentDto) {}
+export class UpdateDocumentDto extends PartialType(CreateDocumentDto) {
+  @ApiProperty({ example: false, required: false })
+  @IsOptional()
+  @Transform(({ value }) => {
+    if (value === 'true' || value === true || value === '1' || value === 1) return true;
+    if (value === 'false' || value === false || value === '0' || value === 0) return false;
+    return value;
+  })
+  @IsBoolean({ message: 'isFeatured phải là giá trị đúng hoặc sai' })
+  isFeatured?: boolean;
+}
